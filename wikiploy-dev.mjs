fe0c021bf31df871a713a017887a1922f1ad7f98
eccenux <eccenux+git@gmail.com>
2024-01-04 20:03:16 +0100

    https://github.com/Eccenux/wikiploy-rollout-example/releases/tag/v1.8

---
+++ wikiploy-dev.mjs
@@ -0,0 +1,23 @@
+import { WikiployLite } from 'wikiploy';
+
+import * as botpass from './bot.config.mjs';
+const ployBot = new WikiployLite(botpass);
+
+// common deploy function(s)
+import { addConfig, setupSummary } from './wikiploy-common.mjs';
+
+// run asynchronously to be able to wait for results
+(async () => {
+	// custom summary from a prompt
+	await setupSummary(ployBot);
+
+	// push out file(s) to wiki
+	const configs = [];
+	addConfig(configs, 'en.wikipedia.org');
+
+	await ployBot.deploy(configs);
+
+})().catch(err => {
+	console.error(err);
+	process.exit(1);
+});
