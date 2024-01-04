3e792be89a0ed3367ac4370bcf3d10812910b376
eccenux <eccenux+git@gmail.com>
2024-01-04 20:26:39 +0100

    v0.0.1 PoC

--- dist/piechartHelper.js
+++ dist/piechartHelper.js
@@ -1,3 +1,4 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 /**
  * Chart helper.
  * 
@@ -76,3 +77,26 @@ PrepChartTitles.prototype.prep = function (el) {
 module.exports = { PrepChartTitles }
 // var prepChartTitles = new PrepChartTitles();
 // prepChartTitles.init();
+
+},{}],2:[function(require,module,exports){
+var { PrepChartTitles } = require("./MyGadget");
+
+// instance
+var gadget = new PrepChartTitles();
+
+// hook when object is ready
+mw.hook('userjs.piechartHelperExample.loaded').fire(gadget);
+
+$(function(){
+	// load dependencies
+	mw.loader.using("oojs-ui-core, oojs-ui.styles.icons-interactions, oojs-ui.styles.icons-layout".split(/, */)).then( function() {
+		mw.hook( 'wikipage.content' ).add( function () {
+			gadget.init();
+			mw.hook('userjs.piechartHelperExample.preped').fire(gadget);
+		} );
+		// hook when initial elements are ready 
+		mw.hook('userjs.piechartHelperExample.ready').fire(gadget);
+	});
+});
+
+},{"./MyGadget":1}]},{},[2]);
--- src/main.js
+++ src/main.js
@@ -1,17 +1,18 @@
-var { MyGadget } = require("./MyGadget");
+var { PrepChartTitles } = require("./MyGadget");
 
 // instance
-var gadget = new MyGadget();
+var gadget = new PrepChartTitles();
 
 // hook when object is ready
 mw.hook('userjs.piechartHelperExample.loaded').fire(gadget);
 
 $(function(){
-	// load Mediwiki core dependency
-	// (in this case util is for `mw.util.addPortletLink`)
-	mw.loader.using(["mediawiki.util"]).then( function() {
-		gadget.init();
-
+	// load dependencies
+	mw.loader.using("oojs-ui-core, oojs-ui.styles.icons-interactions, oojs-ui.styles.icons-layout".split(/, */)).then( function() {
+		mw.hook( 'wikipage.content' ).add( function () {
+			gadget.init();
+			mw.hook('userjs.piechartHelperExample.preped').fire(gadget);
+		} );
 		// hook when initial elements are ready 
 		mw.hook('userjs.piechartHelperExample.ready').fire(gadget);
 	});
